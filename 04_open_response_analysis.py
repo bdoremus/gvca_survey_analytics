51f057b4fe887a78ed4f332382ea29a92ecd1651
ben.doremus <ben.doremus@oliveai.com>
2022-02-26 00:09:10 -0700

    comments for next up projects

--- 04_open_response_analysis.py
+++ 04_open_response_analysis.py
@@ -10,11 +10,30 @@ def main():
     df = pd.read_sql(con=eng,
                      sql="""
                         SELECT question_id,
-                        question_text
+                               question_text,
+                               sub_question_id,
+                               response,
+                               respondent_id,
+                               tenure,
+                               minority,
+                               grammar_conferences,
+                               upper_conferences,
+                               any_support
                         FROM question_open_response
-                            JOIN
-                                question using(question_id)
+                                 JOIN
+                             question using (question_id)
+                                 JOIN
+                             respondents using (respondent_id)
+                        WHERE lower(response) <> 'n/a'
                      """)
+
+    # Create word cloud
+    # Don't bother with sentiment analysis ; we already have discrete sentiment data
+    # Create separate word clouds or other analysis for groups of users
+    #  - sentiment groups
+    #  - grammar/upper
+    #  - all questions together vs separated
+    # manual categorizations/tagging
 
 
 if __name__ == '__main__':
