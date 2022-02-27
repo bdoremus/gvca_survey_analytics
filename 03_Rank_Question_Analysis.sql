904ad1d033ba4082a99cf86b77bcb79e104be540
ben.doremus <ben.doremus@oliveai.com>
2022-02-26 23:30:52 -0700

    chore: cleaner handling of divide by zero for percents, nulls instead of zeros

--- 03_Rank_Question_Analysis.sql
+++ 03_Rank_Question_Analysis.sql
@@ -17,8 +17,8 @@ WITH question_totals AS
                     count(0) FILTER ( WHERE not any_support )::numeric                    as no_support_total,
                     count(0) FILTER ( WHERE tenure = 1)::numeric                          as first_year_family_total,
                     count(0) FILTER ( WHERE tenure > 1)::numeric                          as not_first_year_family_total,
-                    count(0) FILTER ( WHERE tenure <= 3)::numeric                         as third_year_family_total,
-                    count(0) FILTER ( WHERE tenure > 3)::numeric                          as not_third_year_family_total
+                    count(0) FILTER ( WHERE tenure <= 3)::numeric                         as third_or_less_year_family_total,
+                    count(0) FILTER ( WHERE tenure > 3)::numeric                          as more_than_third_year_family_total
 
              FROM question_rank
                       LEFT JOIN
@@ -53,65 +53,36 @@ SELECT question_id,
        question_text,
        response,
        response_description,
-       total,
-       total / question_total                                                       as total_pct,
-       grammar,
-       CASE
-           WHEN grammar_total = 0 THEN 0
-           ELSE grammar / grammar_total END                                         as grammar_pct,
-       grammar_with_conferences,
-       CASE
-           WHEN grammar_with_conferences_total = 0 THEN 0
-           ELSE grammar_with_conferences / grammar_with_conferences_total END       as grammar_with_conferences_pct,
-       grammar_without_conferences,
-       CASE
-           WHEN grammar_without_conferences_total = 0 THEN 0
-           ELSE grammar_without_conferences / grammar_without_conferences_total END as grammar_without_conferences_pct,
-       upper,
-       CASE
-           WHEN upper_total = 0 THEN 0
-           ELSE upper / upper_total END                                             as upper_pct,
-       upper_with_conferences,
-       CASE
-           WHEN upper_with_conferences_total = 0 THEN 0
-           ELSE upper_with_conferences / upper_with_conferences_total END           as upper_with_conferences_pct,
-       upper_without_conferences,
-       CASE
-           WHEN upper_without_conferences_total = 0 THEN 0
-           ELSE upper_without_conferences / upper_without_conferences_total END     as upper_without_conferences_pct,
-       minority,
-       CASE
-           WHEN minority_total = 0 THEN 0
-           ELSE minority / minority_total END                                       as minority_pct,
-       not_minority,
-       CASE
-           WHEN not_minority_total = 0 THEN 0
-           ELSE not_minority / not_minority_total END                               as not_minority_pct,
-       recieves_support,
-       CASE
-           WHEN recieves_support_total = 0 THEN 0
-           ELSE recieves_support / recieves_support_total END                       as recieves_support_pct,
-       no_support,
-       CASE
-           WHEN no_support_total = 0 THEN 0
-           ELSE no_support / no_support_total END                                   as no_support_pct,
-       first_year_family,
-       CASE
-           WHEN first_year_family_total = 0 THEN 0
-           ELSE first_year_family / first_year_family_total END                     as first_year_family_pct,
-       not_first_year_family,
-       CASE
-           WHEN not_first_year_family_total = 0 THEN 0
-           ELSE not_first_year_family / not_first_year_family_total END             as not_first_year_family_pct,
-       third_year_family,
-       CASE
-           WHEN third_year_family_total = 0 THEN 0
-           ELSE third_year_family / third_year_family_total END                     as third_year_family_pct,
-       not_third_year_family,
-       CASE
-           WHEN not_third_year_family_total = 0 THEN 0
-           ELSE not_third_year_family / not_third_year_family_total END             as not_third_year_family_pct
-
+       nullif(total, 0)                                                           as total,
+       total / nullif(question_total, 0)                                          as total_pct,
+       nullif(grammar, 0)                                                         as grammar,
+       grammar / nullif(grammar_total, 0)                                         as grammar_pct,
+       nullif(grammar_with_conferences, 0)                                        as grammar_with_conferences,
+       grammar_with_conferences / nullif(grammar_with_conferences_total, 0)       as grammar_with_conferences_pct,
+       nullif(grammar_without_conferences, 0)                                     as grammar_without_conferences,
+       grammar_without_conferences / nullif(grammar_without_conferences_total, 0) as grammar_without_conferences_pct,
+       nullif(upper, 0)                                                           as upper,
+       upper / nullif(upper_total, 0)                                             as upper_pct,
+       nullif(upper_with_conferences, 0)                                          as upper_with_conferences,
+       upper_with_conferences / nullif(upper_with_conferences_total, 0)           as upper_with_conferences_pct,
+       nullif(upper_without_conferences, 0)                                       as upper_without_conferences,
+       upper_without_conferences / nullif(upper_without_conferences_total, 0)     as upper_without_conferences_pct,
+       nullif(minority, 0)                                                        as minority,
+       minority / nullif(minority_total, 0)                                       as minority_pct,
+       nullif(not_minority, 0)                                                    as not_minority,
+       not_minority / nullif(not_minority_total, 0)                               as not_minority_pct,
+       nullif(recieves_support, 0)                                                as recieves_support,
+       recieves_support / nullif(recieves_support_total, 0)                       as recieves_support_pct,
+       nullif(no_support, 0)                                                      as no_support,
+       no_support / nullif(no_support_total, 0)                                   as no_support_pct,
+       nullif(first_year_family, 0)                                               as first_year_family,
+       first_year_family / nullif(first_year_family_total, 0)                     as first_year_family_pct,
+       nullif(not_first_year_family, 0)                                           as not_first_year_family,
+       not_first_year_family / nullif(not_first_year_family_total, 0)             as not_first_year_family_pct,
+       nullif(third_year_family, 0)                                               as third_year_family,
+       third_year_family / nullif(third_or_less_year_family_total, 0)             as third_or_less_year_family_pct,
+       nullif(not_third_year_family, 0)                                           as not_third_year_family,
+       not_third_year_family / nullif(more_than_third_year_family_total, 0)       as more_than_third_year_family_pct
 FROM response_totals
          INNER JOIN
      question_totals using (question_id)
