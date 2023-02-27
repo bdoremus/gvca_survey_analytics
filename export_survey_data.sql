86a426c5b44ebe2801cda3a8825655db632c645e
Ben Doremus <ben.doremus@magentacc.com>
2023-02-27 15:50:33 -0700

    feat: add export of flattened output

--- export_survey_data.sql
+++ export_survey_data.sql
@@ -81,3 +81,86 @@ FROM all_respondent_questions
      question_open_responses USING (respondent_id, question_id)
 ORDER BY respondent_id, question_id, grammar DESC, middle DESC, upper DESC, whole_school DESC
 ;
+
+
+-- flattened respondent_rank_questions
+WITH duplicated_respondents AS
+         (
+             SELECT respondent_id,
+                    tenure = 1              AS new_family,
+                    minority,
+                    any_support,
+                    grammar_avg IS NOT NULL AS grammar_respondent,
+                    middle_avg IS NOT NULL  AS middle_respondent,
+                    upper_avg IS NOT NULL   AS upper_respondent,
+                    overall_avg             AS avg_score,
+                    soft_delete
+             FROM respondents
+             WHERE NOT soft_delete
+
+             UNION ALL
+
+             -- Get a second rows for any respondent which represented two people
+             SELECT respondent_id,
+                    tenure = 1              AS new_family,
+                    minority,
+                    any_support,
+                    grammar_avg IS NOT NULL AS grammar_respondent,
+                    middle_avg IS NOT NULL  AS middle_respondent,
+                    upper_avg IS NOT NULL   AS upper_respondent,
+                    overall_avg             AS avg_score,
+                    soft_delete
+             FROM respondents
+             WHERE NOT soft_delete
+               AND num_individuals_in_response = 2
+         ),
+     all_respondent_questions AS
+         (
+             SELECT respondent_id,
+                    question_id,
+                    question_text
+             FROM respondents
+                      CROSS JOIN
+                  questions
+             WHERE question_type = 'rank'
+               AND NOT soft_delete
+         ),
+     rank_questions AS
+         (
+             SELECT respondent_id,
+                    question_id,
+                    question_text,
+                    CASE
+                        WHEN grammar THEN 'Grammar'
+                        WHEN middle THEN 'Middle'
+                        WHEN upper THEN 'Upper'
+                        END AS grade_level_for_response,
+                    response_value,
+                    response_text
+             FROM all_respondent_questions
+                      LEFT JOIN
+                  question_rank_responses USING (respondent_id, question_id)
+                      LEFT JOIN
+                  question_response_mapping USING (question_id, response_value)
+         )
+SELECT -- respondents
+       respondent_id,
+       new_family,
+       minority,
+       any_support,
+       grammar_respondent,
+       middle_respondent,
+       upper_respondent,
+       avg_score,
+
+       -- questions
+       question_id,
+       question_text,
+       grade_level_for_response,
+       response_value,
+       response_text
+FROM rank_questions
+         JOIN
+     duplicated_respondents USING (respondent_id)
+ORDER BY respondent_id, question_id, grade_level_for_response
+;
