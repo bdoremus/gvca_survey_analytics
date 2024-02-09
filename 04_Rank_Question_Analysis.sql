c97bb512c50fcf80d370dd60bdc47487bf04e015
Ben Doremus <bdoremus@gmail.com>
2024-02-08 22:55:00 -0700

    chore: update for new year

--- 04_Rank_Question_Analysis.sql
+++ 04_Rank_Question_Analysis.sql
@@ -6,11 +6,7 @@ WITH question_totals AS
              SELECT question_id,
                     count(0)::numeric                                                     as question_total,
                     count(0) FILTER ( WHERE grammar )::numeric                            as grammar_total,
-                    count(0) FILTER ( WHERE grammar_conferences and grammar)::numeric     as grammar_with_conferences_total,
-                    count(0) FILTER ( WHERE not grammar_conferences and grammar)::numeric as grammar_without_conferences_total,
-                    count(0) FILTER ( WHERE upper )::numeric                              as upper_total,
-                    count(0) FILTER ( WHERE upper_conferences and upper)::numeric         as upper_with_conferences_total,
-                    count(0) FILTER ( WHERE not upper_conferences and upper)::numeric     as upper_without_conferences_total,
+                    count(0) FILTER ( WHERE high )::numeric                              as high_total,
                     count(0) FILTER ( WHERE minority )::numeric                           as minority_total,
                     count(0) FILTER ( WHERE not minority )::numeric                       as not_minority_total,
                     count(0) FILTER ( WHERE any_support )::numeric                        as recieves_support_total,
@@ -19,7 +15,7 @@ WITH question_totals AS
                     count(0) FILTER ( WHERE tenure > 1)::numeric                          as not_first_year_family_total,
                     count(0) FILTER ( WHERE tenure <= 3)::numeric                         as third_or_less_year_family_total,
                     count(0) FILTER ( WHERE tenure > 3)::numeric                          as more_than_third_year_family_total
-             FROM question_rank_
+             FROM question_rank_responses
                       LEFT JOIN
                   respondents using (respondent_id)
              GROUP BY question_id
@@ -27,14 +23,10 @@ WITH question_totals AS
      response_totals AS
          (
              SELECT question_id,
-                    response,
+                    response_value,
                     count(0)                                                              as total,
                     count(0) FILTER ( WHERE grammar )                                     as grammar,
-                    count(0) FILTER ( WHERE grammar_conferences and grammar)::numeric     as grammar_with_conferences,
-                    count(0) FILTER ( WHERE not grammar_conferences and grammar)::numeric as grammar_without_conferences,
-                    count(0) FILTER ( WHERE upper )                                       as upper,
-                    count(0) FILTER ( WHERE upper_conferences and upper)::numeric         as upper_with_conferences,
-                    count(0) FILTER ( WHERE not upper_conferences and upper)::numeric     as upper_without_conferences,
+                    count(0) FILTER ( WHERE high )                                       as high,
                     count(0) FILTER ( WHERE minority )                                    as minority,
                     count(0) FILTER ( WHERE not minority )                                as not_minority,
                     count(0) FILTER ( WHERE any_support )                                 as recieves_support,
@@ -43,29 +35,21 @@ WITH question_totals AS
                     count(0) FILTER ( WHERE tenure > 1)::numeric                          as not_first_year_family,
                     count(0) FILTER ( WHERE tenure <= 3)::numeric                         as third_year_family,
                     count(0) FILTER ( WHERE tenure > 3)::numeric                          as not_third_year_family
-             FROM question_rank
+             FROM question_rank_responses
                       LEFT JOIN
                   respondents using (respondent_id)
-             GROUP BY question_id, response
+             GROUP BY question_id, response_value
          )
 SELECT question_id,
        question_text,
-       response,
-       response_description,
+       response_value,
+       response_text,
        nullif(total, 0)                                                           as total,
        total / nullif(question_total, 0)                                          as total_pct,
        nullif(grammar, 0)                                                         as grammar,
        grammar / nullif(grammar_total, 0)                                         as grammar_pct,
-       nullif(grammar_with_conferences, 0)                                        as grammar_with_conferences,
-       grammar_with_conferences / nullif(grammar_with_conferences_total, 0)       as grammar_with_conferences_pct,
-       nullif(grammar_without_conferences, 0)                                     as grammar_without_conferences,
-       grammar_without_conferences / nullif(grammar_without_conferences_total, 0) as grammar_without_conferences_pct,
-       nullif(upper, 0)                                                           as upper,
-       upper / nullif(upper_total, 0)                                             as upper_pct,
-       nullif(upper_with_conferences, 0)                                          as upper_with_conferences,
-       upper_with_conferences / nullif(upper_with_conferences_total, 0)           as upper_with_conferences_pct,
-       nullif(upper_without_conferences, 0)                                       as upper_without_conferences,
-       upper_without_conferences / nullif(upper_without_conferences_total, 0)     as upper_without_conferences_pct,
+       nullif(high, 0)                                                           as high,
+       high / nullif(high_total, 0)                                             as high_pct,
        nullif(minority, 0)                                                        as minority,
        minority / nullif(minority_total, 0)                                       as minority_pct,
        nullif(not_minority, 0)                                                    as not_minority,
@@ -86,36 +70,9 @@ FROM response_totals
          INNER JOIN
      question_totals using (question_id)
          LEFT JOIN
-     question using (question_id)
+     questions using (question_id)
          LEFT JOIN
-     response_definition using (question_id, response)
-ORDER BY question_id, response
+     question_response_mapping using (question_id, response_value)
+ORDER BY question_id, response_value
 ;
 
-
-/*
- Cleaner organization of open response questions
- */
-SELECT
---        question_id,
---        question_text,
---        sub_question_id,
-       response,
-       respondent_id,
-       grammar_avg,
-       upper_avg,
-       overall_avg,
-       tenure,
-       minority,
-       grammar_conferences,
-       upper_conferences,
-       any_support
-FROM question_open_response
-         JOIN
-     question using (question_id)
-         JOIN
-     respondents using (respondent_id)
-WHERE lower(response) <> 'n/a'
-AND question_id = '9'
-AND sub_question_id = 'child'
-;
