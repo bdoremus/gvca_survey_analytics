c9a8193cdbc039689d768c39eb25f5390da0aeaa
Ben Doremus <bdoremus@gmail.com>
2024-02-08 22:54:17 -0700

    feat: update for new year, and data cleaning

--- 03_QA_Checks.sql
+++ 03_QA_Checks.sql
@@ -6,9 +6,11 @@ WITH responses_by_respondents AS
                     rank_response.respondent_id IS NOT NULL AS has_rank_response
              FROM respondents
                       LEFT JOIN
-                      (SELECT DISTINCT respondent_id FROM question_open_responses) AS open_response USING (respondent_id)
+                      (SELECT DISTINCT respondent_id FROM question_open_responses) AS open_response
+                      USING (respondent_id)
                       LEFT JOIN
-                      (SELECT DISTINCT respondent_id FROM question_rank_responses) AS rank_response USING (respondent_id)
+                      (SELECT DISTINCT respondent_id FROM question_rank_responses) AS rank_response
+                      USING (respondent_id)
          )
 UPDATE respondents
 SET soft_delete = TRUE
@@ -31,9 +33,9 @@ FROM respondents
 ;
 
 -- Calculate the "final" completion rate by averaging the max and min families, then dividing by the number of families
-SELECT (386 + 326) / 2. / 418
+SELECT (358 + 298) / 2. / 408
 ;
--- 85.2%
+-- 80.4%
 
 -- Look at those who didn't do any ranked choice, but did do open response.  What were their responses?
 SELECT respondent_id,
@@ -42,17 +44,17 @@ SELECT respondent_id,
        question_text,
        grammar,
        middle,
-       upper,
+       high,
        whole_school,
        response
 FROM respondents
          LEFT JOIN
      question_open_responses USING (respondent_id)
-LEFT JOIN
-    questions using(question_id)
-WHERE overall_avg IS NULL AND NOT soft_delete
+         LEFT JOIN
+     questions using (question_id)
+WHERE overall_avg IS NULL
+  AND NOT soft_delete
 ;
--- Only one person filled out the open response but not the rank questions.  Curious, but fine.
 
 
 -- Look at distribution of open and rank responses by question_id.
@@ -72,11 +74,11 @@ ORDER BY question_id
 -- Ensure each question type is assigned one and only one level
 SELECT *
 FROM question_open_responses
-WHERE grammar::int + middle::int + upper::int + whole_school::int <> 1
+WHERE grammar::int + middle::int + high::int + whole_school::int <> 1
 ;
 SELECT *
 FROM question_rank_responses
-WHERE grammar::int + middle::int + upper::int <> 1
+WHERE grammar::int + middle::int + high::int <> 1
 ;
 
 -- Look at distribution of scores
@@ -93,11 +95,11 @@ SELECT respondent_id,
        any_support,
        grammar_avg,
        middle_avg,
-       upper_avg,
+       high_avg,
        CASE
            WHEN grammar THEN 'Grammar'
            WHEN middle THEN 'Middle'
-           WHEN upper THEN 'Upper'
+           WHEN high THEN 'Upper'
            WHEN whole_school THEN 'All'
            END AS response_category,
        response
@@ -107,3 +109,176 @@ FROM respondents
 WHERE overall_avg < 2
 ORDER BY respondent_id, question_id
 ;
+
+-- Null out some responses
+SELECT response
+FROM question_open_responses
+WHERE response ~* '^\s*(?:n.?a|nothing|none)\s*$'
+;
+UPDATE question_open_responses
+SET response = NULL
+WHERE response ~* '^\s*(?:n.?a|nothing|none)\s*$'
+;
+
+/*******
+  See if we can populate some of the "Same" open response values
+ ******/
+-- Find a regex pattern that works, and filter out ones we don't want to change
+WITH manually_update_responses AS
+         (
+             SELECT DISTINCT respondent_id --, question_id, grammar, middle, high, whole_school, response
+             FROM question_open_responses
+             WHERE length(response) < 50
+               and response ~* 'same|see|above|below'
+               and respondent_id not in ('118519833891', '118525284956', '118520470136') -- "real" responses to leave as-is
+         )
+SELECT respondent_id as resp_id,
+       question_id as q,
+--        question_text,
+       grammar,
+       middle,
+       high,
+       whole_school,
+       response
+FROM question_open_responses
+         JOIN
+     manually_update_responses using (respondent_id)
+JOIN
+    questions using(question_id)
+ORDER BY respondent_id, question_id, grammar desc, middle desc, high desc, whole_school desc
+;
+-- "same same"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118507929834' and question_id = 11 and grammar)
+WHERE respondent_id = '118507929834' and question_id = 11 and whole_school;
+
+-- "same"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118508019950' and question_id = 10 and grammar)
+WHERE respondent_id = '118508019950' and question_id = 10 and whole_school;
+
+-- "same as #12"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118508019950' and question_id = 11 and grammar)
+WHERE respondent_id = '118508019950' and question_id = 11 and whole_school;
+
+-- "Same as above"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118517225382' and question_id = 10 and grammar)
+WHERE respondent_id = '118517225382' and question_id = 10 and whole_school;
+
+-- "See question 12"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118517343801' and question_id = 11 and high)
+WHERE respondent_id = '118517343801' and question_id = 11 and whole_school;
+
+-- "Same as above"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118517606865' and question_id = 11 and grammar)
+WHERE respondent_id = '118517606865' and question_id = 11 and whole_school;
+
+-- "Same as above"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118518045551' and question_id = 10 and grammar)
+WHERE respondent_id = '118518045551' and question_id = 10 and (middle or whole_school);
+
+-- "Same"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118519896064' and question_id = 10 and grammar)
+WHERE respondent_id = '118519896064' and question_id = 10 and whole_school;
+
+-- "Same"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118519896064' and question_id = 11 and grammar)
+WHERE respondent_id = '118519896064' and question_id = 11 and whole_school;
+
+-- "Same as above."
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118520058596' and question_id = 10 and grammar)
+WHERE respondent_id = '118520058596' and question_id = 10 and whole_school;
+
+-- "As above."
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118520538836' and question_id = 10 and grammar)
+WHERE respondent_id = '118520538836' and question_id = 10 and whole_school;
+
+-- "Same as above"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118520916652' and question_id = 10 and grammar)
+WHERE respondent_id = '118520916652' and question_id = 10 and (middle or whole_school);
+
+-- "Same as above"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118522318374' and question_id = 10 and grammar)
+WHERE respondent_id = '118522318374' and question_id = 10 and middle;
+
+-- "Teaches responsibility and above"
+UPDATE question_open_responses
+SET response = response || ': (copied from above) ' || (SELECT response FROM question_open_responses WHERE respondent_id = '118522318374' and question_id = 10 and grammar)
+WHERE respondent_id = '118522318374' and question_id = 10 and high;
+
+-- "The same as above."
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118522535203' and question_id = 10 and high)
+WHERE respondent_id = '118522535203' and question_id = 10 and whole_school;
+
+-- "Same"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118522609410' and question_id = 10 and grammar)
+WHERE respondent_id = '118522609410' and question_id = 10 and whole_school;
+
+-- "Same as #10"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118523470219' and question_id = 10 and middle)
+WHERE respondent_id = '118523470219' and question_id = 10 and whole_school;
+
+-- "Same as 10."
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118525057534' and question_id = 10 and middle)
+WHERE respondent_id = '118525057534' and question_id = 10 and whole_school;
+
+-- "See 12."
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118525057534' and question_id = 11 and middle)
+WHERE respondent_id = '118525057534' and question_id = 11 and whole_school;
+
+-- "See below"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118525060940' and question_id = 10 and whole_school)
+WHERE respondent_id = '118525060940' and question_id = 10 and (middle or high);
+
+-- "as above"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118525095561' and question_id = 10 and high)
+WHERE respondent_id = '118525095561' and question_id = 10 and whole_school;
+
+-- "as above"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118525095561' and question_id = 11 and high)
+WHERE respondent_id = '118525095561' and question_id = 11 and whole_school;
+
+-- "Same as above."
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118525243547' and question_id = 11 and grammar)
+WHERE respondent_id = '118525243547' and question_id = 11 and whole_school;
+
+-- "Same answer as 12"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118525286703' and question_id = 11 and middle)
+WHERE respondent_id = '118525286703' and question_id = 11 and whole_school;
+
+-- "Same"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118527494581' and question_id = 10 and grammar)
+WHERE respondent_id = '118527494581' and question_id = 10 and whole_school;
+
+-- "See above"
+UPDATE question_open_responses
+SET response = (SELECT response FROM question_open_responses WHERE respondent_id = '118527870497' and question_id = 10 and grammar)
+WHERE respondent_id = '118527870497' and question_id = 10 and whole_school;
+
+
+-- Final check
+SELECT response
+FROM question_open_responses
+ORDER BY length(response)
