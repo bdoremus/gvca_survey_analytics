26c3a34ce841a41d6847eca46e49b56415c430e6
Ben Doremus <bdoremus@gmail.com>
2024-02-28 00:51:38 -0700

    feat: demographic breakdown of rank questions

--- 04_Rank_Question_Charts.py
+++ 04_Rank_Question_Charts.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.engine.base import Engine as sqlalchemy_Engine
 
@@ -12,7 +13,8 @@ def query_to_bar_chart(conn: sqlalchemy_Engine,
                        title: str,
                        x_axis_label: str,
                        x_data_label_query: str,
-                       proportion_query: str
+                       proportion_query: str,
+                       subfolder: Path = None
                        ) -> None:
     """
     Execute two queries and modify results to feed the creation of a stacked bar chart.
@@ -21,6 +23,7 @@ def query_to_bar_chart(conn: sqlalchemy_Engine,
     :param x_axis_label:
     :param x_data_label_query:
     :param proportion_query:
+    :param subfolder:
     :return:
     """
 
@@ -28,10 +31,10 @@ def query_to_bar_chart(conn: sqlalchemy_Engine,
     proportions = pd.read_sql(con=conn, sql=proportion_query)
 
     create_stacked_bar_chart(title=title, x_axis_label=x_axis_label, x_data_labels=x_data_labels,
-                             proportions=proportions)
+                             proportions=proportions, subfolder=subfolder)
 
 
-def create_stacked_bar_chart(title: str, x_axis_label: str, x_data_labels: list, proportions: pd.DataFrame) -> None:
+def create_stacked_bar_chart(title: str, x_axis_label: str, x_data_labels: list, proportions: pd.DataFrame, subfolder: Path = None) -> None:
     """
     Save a stacked bar chart to ./artifacts/
 
@@ -40,6 +43,7 @@ def create_stacked_bar_chart(title: str, x_axis_label: str, x_data_labels: list,
     :param x_data_labels:
     :param proportions: {bottom_color_in_each_bar: [col1, col2, col3...],
                          second_from_bottom_color_in_each_bar: [col1, col2, col3...], ...}
+    :param subfolder: Optional, otherwise use the title
     :return:
     """
     r1 = proportions[proportions.response_value == 1].pct.values.tolist()[0]
@@ -59,7 +63,7 @@ def create_stacked_bar_chart(title: str, x_axis_label: str, x_data_labels: list,
     ax.set_ylabel("Proportion")
 
     plt.tight_layout()
-    plt.savefig(f'artifacts/{title}', transparent=True)
+    plt.savefig(subfolder / title if subfolder else f'artifacts/{title}', transparent=True)
     plt.show()
 
 
@@ -273,19 +277,22 @@ def question_demographic_breakouts(conn):
         elif question_id == 8:
             summarized_text = "Communication with school leadership"
 
-        # by_grade_level(conn, question_id, summarized_text)
+        by_grade_level(conn, question_id, summarized_text)
+        by_support_summary(conn, question_id, summarized_text)
         by_minority_summary(conn, question_id, summarized_text)
-        # by_support_summary(conn, question_id, summarized_text)
-        # by_first_year_family_summary(conn, question_id, summarized_text)
+        by_first_year_family_summary(conn, question_id, summarized_text)
 
 
 def by_grade_level(conn, question_id, summarized_text):
     """
     Given a question_id, create a chart breaking out each grade into its own column
     """
+    subfolder = Path('artifacts/Rank Response - Grade Level')
+    subfolder.mkdir(parents=True, exist_ok=True)
     query_to_bar_chart(
         conn=conn,
         title=f'{question_id}: ' + summarized_text,
+        subfolder=subfolder,
         x_axis_label='Grade Level',
         x_data_label_query=f"""
             SELECT CASE WHEN grammar THEN 'Grammar'
@@ -357,92 +364,247 @@ def by_grade_level(conn, question_id, summarized_text):
     )
 
 
-def by_minority_summary(conn, question_id, summarized_text):
+def by_support_summary(conn, question_id, summarized_text):
     """
-    Given a question_id, create a chart breaking out each grade into its own column
+    Given a question_id, create a chart breaking out students who received support services from those who did not
     """
+    subfolder = Path('artifacts/Rank Response - Student Services')
+    subfolder.mkdir(parents=True, exist_ok=True)
     query_to_bar_chart(
         conn=conn,
         title=f'{question_id}: ' + summarized_text,
+        subfolder=subfolder,
         x_axis_label='Grade Level',
-        x_data_label_query="""
-            SELECT CASE WHEN grammar THEN 'Grammar'
-                        WHEN middle THEN 'Middle'
-                        WHEN high THEN 'High'
-                        END || 
-                        E'\n(' ||
-                        ROUND(
-                            SUM(response_value * num_individuals_in_response)::NUMERIC /
-                                SUM(num_individuals_in_response),
-                            2) || 
-                        ')' AS title
+        x_data_label_query=f"""
+            SELECT CASE
+                       WHEN respondents.any_support THEN 'Received Support'
+                       WHEN NOT any_support THEN 'Did not Receive Support'
+                       ELSE 'Did not answer'
+                       END ||
+                   E'\n(' ||
+                   ROUND(SUM(response_value * num_individuals_in_response)::NUMERIC /
+                            SUM(num_individuals_in_response),
+                         2) ||
+                   ')' AS title
             FROM question_rank_responses
                      JOIN
                  respondents USING (respondent_id)
-            WHERE question_id = 4
-            GROUP BY grammar, middle, high
-            ORDER BY grammar DESC, middle DESC, high DESC
+            WHERE question_id = {question_id}
+            GROUP BY any_support
+            ORDER BY any_support DESC NULLS LAST
                 """,
         proportion_query=f"""
             WITH expected_values AS
                      (
-                         SELECT levels.column1          AS level,
-                                levels.column2          AS level_order,
+                         SELECT demographics.column1    AS demographic,
+                                demographics.column2    AS demographic_order, -- any_support desc nulls last
                                 response_values.column1 AS response_value
-                         FROM (VALUES ('Grammar', 1), ('Middle', 2), ('High', 3)) AS levels,
+                         FROM (VALUES ('Received Support', 1),
+                                      ('Did not Receive Support', 2),
+                                      ('Did not answer', 3)) AS demographics,
                               (VALUES (1), (2), (3), (4)) AS response_values
                      ),
-                 sum_by_grade AS
+                 sum_by_demographic AS
                      (
                          SELECT CASE
-                                    WHEN grammar THEN 'Grammar'
-                                    WHEN middle THEN 'Middle'
-                                    WHEN high THEN 'High'
-                                    END                          AS level,
+                                    WHEN any_support THEN 'Received Support'
+                                    WHEN NOT any_support THEN 'Did not Receive Support'
+                                    ELSE 'Did not answer'
+                                    END                          AS demographic,
                                 response_value,
                                 SUM(num_individuals_in_response) AS num_responses
                          FROM question_rank_responses
                                   JOIN
                               respondents USING (respondent_id)
                          WHERE question_id = {question_id}
-                         GROUP BY level, response_value
+                         GROUP BY demographic, response_value
                      ),
                  fill_in_blanks AS
                      (
-                         -- If a level doesn't have any responses, fill it in as 0
-                         SELECT level,
-                                level_order,
+                         -- If a demographic doesn't have any responses, fill it in as 0
+                         SELECT demographic,
+                                demographic_order,
                                 response_value,
                                 COALESCE(num_responses, 0) AS num_responses
                          FROM expected_values
                                   LEFT JOIN
-                              sum_by_grade USING (level, response_value)
+                              sum_by_demographic USING (demographic, response_value)
                      ),
                  sum_w_totals AS
                      (
-                         SELECT level,
+                         SELECT demographic,
                                 SUM(num_responses) AS total
                          FROM fill_in_blanks
-                         GROUP BY level
+                         GROUP BY demographic
                      )
             SELECT response_value,
-                   ARRAY_AGG(num_responses::NUMERIC / total ORDER BY level_order) AS pct
+                   ARRAY_AGG(num_responses::NUMERIC / total ORDER BY demographic_order) AS pct
             FROM fill_in_blanks
                      JOIN
-                 sum_w_totals USING (level)
+                 sum_w_totals USING (demographic)
             GROUP BY response_value
             ;"""
     )
 
 
-def by_support_summary(conn, question_id, summarized_text):
-    # TODO: todo
-    pass
+def by_minority_summary(conn, question_id, summarized_text):
+    subfolder = Path('artifacts/Rank Response - Minority')
+    subfolder.mkdir(parents=True, exist_ok=True)
+    query_to_bar_chart(
+        conn=conn,
+        title=f'{question_id}: ' + summarized_text,
+        subfolder=subfolder,
+        x_axis_label='Grade Level',
+        x_data_label_query=f"""
+            SELECT CASE
+                       WHEN minority THEN 'Minority'
+                       WHEN NOT minority THEN 'Not Minority'
+                       ELSE 'Did not answer'
+                       END ||
+                   E'\n(' ||
+                   ROUND(SUM(response_value * num_individuals_in_response)::NUMERIC /
+                            SUM(num_individuals_in_response),
+                         2) ||
+                   ')' AS title
+            FROM question_rank_responses
+                     JOIN
+                 respondents USING (respondent_id)
+            WHERE question_id = {question_id}
+            GROUP BY minority
+            ORDER BY minority DESC NULLS LAST
+                    """,
+        proportion_query=f"""
+                WITH expected_values AS
+                         (
+                             SELECT demographics.column1    AS demographic,
+                                    demographics.column2    AS demographic_order, -- any_support desc nulls last
+                                    response_values.column1 AS response_value
+                             FROM (VALUES ('Minority', 1),
+                                          ('Not Minority', 2),
+                                          ('Did not answer', 3)) AS demographics,
+                                  (VALUES (1), (2), (3), (4)) AS response_values
+                         ),
+                     sum_by_demographic AS
+                         (
+                             SELECT CASE
+                                        WHEN minority THEN 'Minority'
+                                        WHEN NOT minority THEN 'Not Minority'
+                                        ELSE 'Did not answer'
+                                        END                          AS demographic,
+                                    response_value,
+                                    SUM(num_individuals_in_response) AS num_responses
+                             FROM question_rank_responses
+                                      JOIN
+                                  respondents USING (respondent_id)
+                             WHERE question_id = {question_id}
+                             GROUP BY demographic, response_value
+                         ),
+                     fill_in_blanks AS
+                         (
+                             -- If a demographic doesn't have any responses, fill it in as 0
+                             SELECT demographic,
+                                    demographic_order,
+                                    response_value,
+                                    COALESCE(num_responses, 0) AS num_responses
+                             FROM expected_values
+                                      LEFT JOIN
+                                  sum_by_demographic USING (demographic, response_value)
+                         ),
+                     sum_w_totals AS
+                         (
+                             SELECT demographic,
+                                    SUM(num_responses) AS total
+                             FROM fill_in_blanks
+                             GROUP BY demographic
+                         )
+                SELECT response_value,
+                       ARRAY_AGG(num_responses::NUMERIC / total ORDER BY demographic_order) AS pct
+                FROM fill_in_blanks
+                         JOIN
+                     sum_w_totals USING (demographic)
+                GROUP BY response_value
+                ;"""
+    )
 
 
 def by_first_year_family_summary(conn, question_id, summarized_text):
-    # TODO: todo
-    pass
+    subfolder = Path('artifacts/Rank Response - First Year Families')
+    subfolder.mkdir(parents=True, exist_ok=True)
+    query_to_bar_chart(
+        conn=conn,
+        title=f'{question_id}: ' + summarized_text,
+        subfolder=subfolder,
+        x_axis_label='Grade Level',
+        x_data_label_query=f"""
+            SELECT CASE
+                       WHEN tenure = 1 THEN 'First Year Family'
+                       WHEN not tenure = 1 THEN 'Returning Family'
+                       ELSE 'Did not answer'
+                       END ||
+                   E'\n(' ||
+                   ROUND(SUM(response_value * num_individuals_in_response)::NUMERIC /
+                            SUM(num_individuals_in_response),
+                         2) ||
+                   ')' AS title
+            FROM question_rank_responses
+                     JOIN
+                 respondents USING (respondent_id)
+            WHERE question_id = {question_id}
+            GROUP BY tenure = 1
+            ORDER BY tenure = 1 DESC NULLS LAST
+                    """,
+        proportion_query=f"""
+                WITH expected_values AS
+                         (
+                             SELECT demographics.column1    AS demographic,
+                                    demographics.column2    AS demographic_order, -- any_support desc nulls last
+                                    response_values.column1 AS response_value
+                             FROM (VALUES ('First Year Family', 1),
+                                          ('Returning Family', 2),
+                                          ('Did not answer', 3)) AS demographics,
+                                  (VALUES (1), (2), (3), (4)) AS response_values
+                         ),
+                     sum_by_demographic AS
+                         (
+                             SELECT CASE
+                                        WHEN tenure = 1 THEN 'First Year Family'
+                                        WHEN NOT tenure = 1 THEN 'Returning Family'
+                                        ELSE 'Did not answer'
+                                        END                          AS demographic,
+                                    response_value,
+                                    SUM(num_individuals_in_response) AS num_responses
+                             FROM question_rank_responses
+                                      JOIN
+                                  respondents USING (respondent_id)
+                             WHERE question_id = {question_id}
+                             GROUP BY tenure = 1, response_value
+                         ),
+                     fill_in_blanks AS
+                         (
+                             -- If a demographic doesn't have any responses, fill it in as 0
+                             SELECT demographic,
+                                    demographic_order,
+                                    response_value,
+                                    COALESCE(num_responses, 0) AS num_responses
+                             FROM expected_values
+                                      LEFT JOIN
+                                  sum_by_demographic USING (demographic, response_value)
+                         ),
+                     sum_w_totals AS
+                         (
+                             SELECT demographic,
+                                    SUM(num_responses) AS total
+                             FROM fill_in_blanks
+                             GROUP BY demographic
+                         )
+                SELECT response_value,
+                       ARRAY_AGG(num_responses::NUMERIC / total ORDER BY demographic_order) AS pct
+                FROM fill_in_blanks
+                         JOIN
+                     sum_w_totals USING (demographic)
+                GROUP BY response_value
+                ;"""
+    )
 
 
 def q5_student_services(conn):
@@ -596,9 +758,9 @@ def main():
     with create_engine(DATABASE_CONNECTION_STRING).connect() as conn:
         conn.execute(f"SET SCHEMA '{DATABASE_SCHEMA}'")
 
-        # create_question_summary(conn)
-        # create_grade_summary(conn)
-        # q5_student_services(conn)
+        create_question_summary(conn)
+        create_grade_summary(conn)
+        q5_student_services(conn)
         # yoy_total_diff(conn)
         question_demographic_breakouts(conn)
 
